f3a204b055d3d923f0cab2453ad708ee88fab6f9
Fernando Kitakawa <ferkitakawa123@gmail.com>
2024-08-27 00:27:00 -0300

    Starting to implement the UserRepository with all CRUD's method.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserRepository],
+  imports:[DbModule]
 })
 export class UserModule {}
